33ac240d2382ac24e40353cf3f819f3814f28370
Kurt <Kurt@10.4.2.56>
2016-03-01 08:38:53 -0600

    updating assignment details

---
+++ cdCollection/src/cdcollection/CdCollection.java
@@ -0,0 +1,16 @@
+package cdcollection;
+
+/**
+ *
+ * @author Insert your name here
+ */
+public class CdCollection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
